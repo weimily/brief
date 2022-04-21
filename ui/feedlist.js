449bb09cd7ff7d5cc94f72c67efa5fe3e121e320
Degit22 <degit22@gmail.com>
2022-04-21 22:53:41 +0700

    Cleanup database

--- ui/feedlist.js
+++ ui/feedlist.js
@@ -1173,6 +1173,7 @@ export let Commands = {
 
     emptyTrash: function cmd_emptyTrash() {
         ViewList.db.query(ViewList.getQueryForView('trash-folder')).markDeleted('deleted');
+        ViewList.db.cleanupEntries();
     },
 
     toggleSelectedEntryRead: function cmd_toggleSelectedEntryRead() {
