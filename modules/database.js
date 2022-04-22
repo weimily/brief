64bd8e8ce471ec281ef37bd33926ac06d3d79757
Degit22 <degit22@gmail.com>
2022-04-23 02:01:47 +0700

    cleanup revisions

--- modules/database.js
+++ modules/database.js
@@ -924,6 +924,10 @@ export class Database {
             let tx = this.db().transaction(['entries'], 'readwrite');
             let request = tx.objectStore('entries').delete(id);
             DbUtil.requestPromise(request);
+
+            let tx2 = this.db().transaction(['revisions'], 'readwrite');
+            let request2 = tx2.objectStore('revisions').delete(id);
+            DbUtil.requestPromise(request2);
         }
     }
 }
